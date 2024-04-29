a9f19cf1e509f3d7bce85090128c918d54cea026
ChitreshN <chitreshnarra@gmail.com>
2024-04-30 01:38:49 +0530

    feat: skeleton of all the functions in user page

--- web/dbms/library/user_views.py
+++ web/dbms/library/user_views.py
@@ -18,7 +18,6 @@ def user(req, id, emp_id=''):
 
         # Assuming reading_history and private_list are retrieved from the database
         currently_borrowed = [("Book1", 1)]
-        private_list = [("Book1", 1)]
         reading_history = [("Book1", 1)]
 
     context = {
@@ -26,18 +25,28 @@ def user(req, id, emp_id=''):
         'last_name': last_name,
         'reading_history': reading_history,
         'currently_borrowed': currently_borrowed,
-        'private_list': private_list
     }
     return render(req, 'user_dashboard.html', context)
 
 
-def users(req):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT first_name from members")
-        row = cursor.fetchall()
-        if row:
-            first = row
-            return HttpResponse(f"{first}")
+def return_page(req, id):
+    return HttpResponse("borrow list with an option to return")
+
+
+def book_return_page(req, id, book_id):
+    return HttpResponse("page to return book")
+
+
+def borrow_page(req):
+    return HttpResponse("books available to borrow from")
+
+
+def book_request_page(req, id, book_id):
+    return HttpResponse("insert entry into request table")
+
+
+def books_currently_borrowed(req, id):
+    return HttpResponse("show details of books that are currently borrowed and fines if any")
 
 
 def books(req, user_id, book_id):
