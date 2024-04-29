b391e68afa29d810d5ecd24077cbd64147edb2f0
ChitreshN <chitreshnarra@gmail.com>
2024-04-30 01:38:06 +0530

    feat: skeleton of all the functions in emp page

--- web/dbms/library/employee_views.py
+++ web/dbms/library/employee_views.py
@@ -16,8 +16,29 @@ def employee(req, id):
     return render(req, 'employee_dashboard.html', context)
 
 
+def requests(req, emp_id):
+    return HttpResponse("view borrow requests for his branch")
+
+
+def accept_requests(req, emp_id, req_id):
+    return HttpResponse("insert data into issue table")
+
+
+def user_list(req, emp_id):
+    return HttpResponse("page to view all users of a branch")
+
+
+def user_info(req, user_id):
+    return HttpResponse("page to view fines and books borrowed by a particular user")
+
+
+def fine_recieved(req, user_id):
+    return HttpResponse("update fine recieved in the issue table")
+
+
 def issued(req, id):
     # query should return books issued by the said employee
+    # and their status
     with connection.cursor() as cursor:
         cursor.execute("query that gives books issued by the employee", [id])
         row = cursor.fetchall()
